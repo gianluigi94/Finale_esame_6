7e6cb89854cce20d6f89b993f42179c759dc2fb9
GianluigiAbbracciavento <gianluigiabbracciavento@yahoo.com>
2026-01-13 08:03:48 +0100

    metà commenti

--- src/app/_benvenuto/benvenuto-routing.module.ts
+++ src/app/_benvenuto/benvenuto-routing.module.ts
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+// Modulo di routing di 'Benvenuto' che definisce le rotte per la schermata di welcome, applico guardie per le animazioni.
+
+>>>>>>> 35cb057 (metà commenti)
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -6,10 +11,18 @@ import { LoginUscitaGuard } from '../_guard/login-uscita.guard';
 
 const routes: Routes = [
   {
+<<<<<<< HEAD
+=======
+    //se il path è vuoto vengo reidirizzato al componente di bevenuto, /benvenuto
+>>>>>>> 35cb057 (metà commenti)
     path: '',
     component: WelcomeComponent,
   },
   {
+<<<<<<< HEAD
+=======
+    //se il path è /benvenuto/login entrano in gioco il componente di login e una guard che si accorge quando da da benvenuto/login ritorno a solo /benvenuto e mostra le animazioni al ritroso
+>>>>>>> 35cb057 (metà commenti)
     path: 'login',
     component: LoginComponent,
     canDeactivate: [LoginUscitaGuard],
@@ -17,7 +30,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
+<<<<<<< HEAD
   imports: [RouterModule.forChild(routes)],
+=======
+   imports: [RouterModule.forChild(routes)], // registro queste rotte come rotte figlie del modulo, così posso collegarle al router dell'app
+>>>>>>> 35cb057 (metà commenti)
   exports: [RouterModule],
 })
 export class BenvenutoRoutingModule {}
--- src/app/_benvenuto/benvenuto.module.ts
+++ src/app/_benvenuto/benvenuto.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+<<<<<<< HEAD
 import { TranslateModule } from '@ngx-translate/core'; // 👈 aggiungi questo
 
+=======
+import { TranslateModule } from '@ngx-translate/core';
+>>>>>>> 35cb057 (metà commenti)
 import { BenvenutoRoutingModule } from './benvenuto-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
@@ -16,8 +20,13 @@ import { SaturnoModule } from '../_componenti_comuni/saturno/saturno.module';
   imports: [
     CommonModule,
     BenvenutoRoutingModule,
+<<<<<<< HEAD
     ComponentiComuniModule,
     SaturnoModule,
+=======
+    ComponentiComuniModule, //utilizzo dei componenti comuni
+    SaturnoModule, //utilizzo di saturno
+>>>>>>> 35cb057 (metà commenti)
     ReactiveFormsModule,
     TranslateModule,
   ],
